5b229b29ff06362e1f1a020f7fabfa2a29e5fe96
Alejandro Iglesias <alejandroiestevez@gmail.com>
2026-06-25 14:28:38 +0200

    feat: complete users app with admin registration

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Panel de administración personalizado para el modelo User.
+    """
+
+    list_display = ["username", "email", "fecha_registro", "is_staff"]
+    search_fields = ["username", "email"]
