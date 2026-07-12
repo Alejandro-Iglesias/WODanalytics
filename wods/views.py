7eafb1f8de9d7d39ee31ad7df18417e9cffa2ecb
Alejandro Iglesias <alejandroiestevez@gmail.com>
2026-07-12 19:56:54 +0200

    feat: integrate services into views

--- wods/views.py
+++ wods/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Wod, MetricaRecuperacion
 from .serializers import WodSerializer, MetricaRecuperacionSerializer
+from .services import calcular_media_semanal, calcular_racha, evaluar_alerta_fatiga
 
 
 class WodListCreateView(generics.ListCreateAPIView):
@@ -21,6 +23,23 @@ class WodListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         # Asignamos el usuario autenticado automáticamente al crear un WOD
         serializer.save(usuario=self.request.user)
+
+    def list(self, request):
+        racha = calcular_racha(request.user)
+        media = calcular_media_semanal(request.user)
+        fatiga = evaluar_alerta_fatiga(request.user)
+
+        queryset = self.get_queryset()
+        serializer = WodSerializer(queryset, many=True)
+
+        return Response(
+            {
+                "racha_dias": racha,
+                "media_semanal": media,
+                "alerta_fatiga": fatiga,
+                "wods": serializer.data,
+            }
+        )
 
 
 class MetricaRecuperacionListCreateView(generics.ListCreateAPIView):
